df864cefcfbac649298855183b984c70ec7ad6bc
wissalaitihya <wissalaitihya@gmail.com>
2026-06-25 16:58:34 +0100

    add CRUD with API resources and form requests

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlueprintController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
    Route::post('/logout', [AuthController::class, 'logout']);
+   Route::apiResource('blueprints', BlueprintController::class);
 });
